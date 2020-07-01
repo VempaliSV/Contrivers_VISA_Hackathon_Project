ba6764803b9e8bcabe21d848b902eb52b6a8c1ad
Ravi Kumar Singh <imrk016@gmail.com>
2020-07-01 17:05:27 +0530

    deleting

--- ONE VISA CONNECT/Customer/Android App/my app/backend/wallet_api/default_config.py
+++
@@ -1,6 +0,0 @@
-DEBUG = True
-SQLALCHEMY_DATABASE_URI = "sqlite:///data.db"
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-PROPAGATE_EXCEPTIONS = True
-SECRET_KEY = "change-this-key-in-the-application-config"
-JWT_SECRET_KEY = "change-this-key-to-something-different-in-the-application-config"
